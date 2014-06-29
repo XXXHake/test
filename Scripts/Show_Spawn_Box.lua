6759f52b039c557867b876af0c9b320bb1d9c9d4
DeadlyLone <SkyIce39@gmail.com>
2014-06-29 20:40:41 +0300

    Update Show_Spawn_Box.lua

--- Scripts/Show_Spawn_Box.lua
+++ Scripts/Show_Spawn_Box.lua
@@ -2,17 +2,26 @@ require("libs.Utils")
 
 spots = {
 --radian
---{a.x,a.y,c.x,c.y}
 {2240,-4288,3776,-5312}, -- easy
 {2688,-2944, 3776,-4096}, -- hard near rune
 {1088,-3200,2304,-4544}, -- medium near rune
 }
 
+local toggleKey = string.byte("L")
+
+local eff = {}
+local eff1 = {}
+local eff2 = {}
+local eff3 = {}
+local eff4 = {}
+
+local effec = "candle_flame_medium" -- ambient_gizmo_model
+
 --[[
 
 	a----b
-	|    |
-	|    |
+	|	 |
+	|	 |
 	d----c
 	
 	a 2240;-4288
@@ -20,21 +29,15 @@ spots = {
 
 ]]
 
-local eff = {}
-local eff1 = {} local eff2 = {}
-local eff3 = {} local eff4 = {}
+function Key(msg,code)
 
-local effec = "candle_flame_medium" -- ambient_gizmo_model
-
-function Tick(tick)
-
-	if not client.connected or client.loading or client.console or not SleepCheck() then return end	
-	
-	Sleep(500)
+	if msg ~= KEY_UP or code ~= toggleKey or client.chat or not client.connected or client.loading or client.console then
+    	return
+    end
 	
 	local me = entityList:GetMyHero()
+	
 	if not me then return end
-
 
 	for i,k in ipairs(spots) do
 		
@@ -42,19 +45,19 @@ function Tick(tick)
 		
 		if (GetDistance2D(Vector(k[1],k[2],0),vec) < 1500 or GetDistance2D(Vector(k[3],k[4],0),vec) < 1500) and not eff[i] then
 			
-			local coint1 = math.floor(math.floor(k[3]-k[1])/100)
-			local coint2 = math.floor(math.floor(k[2]-k[4])/100)	
+			local coint1 = math.floor(math.floor(k[3]-k[1])/50)
+			local coint2 = math.floor(math.floor(k[2]-k[4])/50)
 			
 			eff[i] = {}		
 			
-			for a = 1,20 do
+			for a = 1,40 do
 				eff[i].eff1 = {} eff[i].eff2 = {}	
 				eff[i].eff3 = {} eff[i].eff4 = {}
 			end		
 			
 			for a = 1,coint1 do
-				local first = Vector(k[1]+a*100, k[4], 0)
-				local second = Vector(k[1]+a*100, k[2], 0)				
+				local first = Vector(k[1]+a*50, k[4], 0)
+				local second = Vector(k[1]+a*50, k[2], 0)				
 				eff[i].eff1[a] = Effect(first,effec)
 				eff[i].eff1[a]:SetVector(0,GetVector(first))				
 				eff[i].eff3[a] = Effect(second,effec)
@@ -62,15 +65,15 @@ function Tick(tick)
 			end
 			
 			for a = 1,coint2 do		
-				local first = Vector(k[1], k[4]+a*100, 0)
-				local second = Vector(k[3], k[4]+a*100, 0)				
+				local first = Vector(k[1], k[4]+a*50, 0)
+				local second = Vector(k[3], k[4]+a*50, 0)				
 				eff[i].eff2[a] = Effect(first,effec)
 				eff[i].eff2[a]:SetVector(0,GetVector(first))				
 				eff[i].eff4[a] = Effect(second,effec)
 				eff[i].eff4[a]:SetVector(0,GetVector(second))
 			end
 			
-		elseif GetDistance2D(Vector(k[1],k[2],0),vec) > 1500 and GetDistance2D(Vector(k[3],k[4],0),vec) > 1500 and eff[i] then
+		elseif eff[i] then
 			eff[i] = nil
 			collectgarbage("collect")
 		end
@@ -89,10 +92,12 @@ end
 
 function GameClose()
 	eff = {}
-	eff1 = {} eff2 = {}
-	eff3 = {} eff4 = {}
+	eff1 = {}
+	eff2 = {}
+	eff3 = {}
+	eff4 = {}
 	collectgarbage("collect")
 end
 
-script:RegisterEvent(EVENT_CLOSE, GameClose)
-script:RegisterEvent(EVENT_TICK,Tick)
+script:RegisterEvent(EVENT_CLOSE,GameClose)
+script:RegisterEvent(EVENT_KEY,Key)
